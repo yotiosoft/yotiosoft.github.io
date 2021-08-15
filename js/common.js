1d14dd7f673b5428290c6693de8247c17ea1b58e
YotioSoft <ytani0323@gmail.com>
2021-08-16 00:01:27 +0900

    画面幅変更時にモバイル版/PC版を切り替え

--- js/common.js
+++ js/common.js
@@ -1,21 +1,25 @@
 //共通パーツ読み込み
 $(function() {
-    if (window.innerWidth <= 768　|| navigator.userAgent.match(/iPhone|Android.+Mobile/)) {
-		var link = document.createElement('link');
-		link.href = '/style_m.css';
-		link.rel = 'stylesheet';
-		link.type = 'text/css';
+    window.addEventListener('DOMContentLoaded', function(){
+        window.addEventListener('resize', function(){
+            if (window.innerWidth <= 768　|| navigator.userAgent.match(/iPhone|Android.+Mobile/)) {
+                var link = document.createElement('link');
+                link.href = '/style_m.css';
+                link.rel = 'stylesheet';
+                link.type = 'text/css';
 
-        $("#header").load("/header_m.html");
-        $("#footer").load("/footer_m.html");
-    }
-    else {
-		var link = document.createElement('link');
-		link.href = '/style.css';
-		link.rel = 'stylesheet';
-		link.type = 'text/css';
+                $("#header").load("/header_m.html");
+                $("#footer").load("/footer_m.html");
+            }
+            else {
+                var link = document.createElement('link');
+                link.href = '/style.css';
+                link.rel = 'stylesheet';
+                link.type = 'text/css';
 
-        $("#header").load("/header.html");
-        $("#footer").load("/footer.html");
-    }
+                $("#header").load("/header.html");
+                $("#footer").load("/footer.html");
+            }
+        });
+    });
 });
