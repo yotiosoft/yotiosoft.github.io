67fc634c659d26ef5c614bbbd539ec2e27a45035
YotioSoft <ytani0323@gmail.com>
2021-08-16 02:27:03 +0900

    768px以上をPC版に指定 / スタイルシート読み込みをhtmlに

--- js/common.js
+++ js/common.js
@@ -1,40 +1,20 @@
 //共通パーツ読み込み
 $(function() {
-    if (window.innerWidth <= 768　|| navigator.userAgent.match(/iPhone|Android.+Mobile/)) {
-        var link = document.createElement('link');
-        link.href = '/style_m.css';
-        link.rel = 'stylesheet';
-        link.type = 'text/css';
-
+    if (window.innerWidth <= 767　|| navigator.userAgent.match(/iPhone|Android.+Mobile/)) {
         $("#header").load("/header_m.html");
         $("#footer").load("/footer_m.html");
     }
     else {
-        var link = document.createElement('link');
-        link.href = '/style.css';
-        link.rel = 'stylesheet';
-        link.type = 'text/css';
-
         $("#header").load("/header.html");
         $("#footer").load("/footer.html");
     }
     
     window.addEventListener('resize', function(){
-        if (window.innerWidth <= 768　|| navigator.userAgent.match(/iPhone|Android.+Mobile/)) {
-            var link = document.createElement('link');
-            link.href = '/style_m.css';
-            link.rel = 'stylesheet';
-            link.type = 'text/css';
-
+        if (window.innerWidth <= 767　|| navigator.userAgent.match(/iPhone|Android.+Mobile/)) {
             $("#header").load("/header_m.html");
             $("#footer").load("/footer_m.html");
         }
         else {
-            var link = document.createElement('link');
-            link.href = '/style.css';
-            link.rel = 'stylesheet';
-            link.type = 'text/css';
-
             $("#header").load("/header.html");
             $("#footer").load("/footer.html");
         }
