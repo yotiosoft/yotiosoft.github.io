ab29b065223b5096a2058fa1c5c93ba8b0e1034f
YotioSoft <ytani0323@gmail.com>
2021-08-15 23:18:32 +0900

    common.jsを修正

--- js/common.js
+++ js/common.js
@@ -1,11 +1,11 @@
 //共通パーツ読み込み
 $(function() {
-    if (window.innerWidth > 768) {
-        $("#header").load("/header.html");
-        $("#footer").load("/footer.html");
-    }
-    else {
+    if (navigator.userAgent.match(/iPhone|Android.+Mobile/)) {
         $("#header").load("/header_m.html");
         $("#footer").load("/footer_m.html");
+    }
+    else {
+        $("#header").load("/header.html");
+        $("#footer").load("/footer.html");
     }
 });
