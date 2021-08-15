2d701c945f0b9fc3b6d34c9b63725397c9fd7ca9
YotioSoft <ytani0323@gmail.com>
2021-08-16 00:05:02 +0900

    修正: common.js

--- js/common.js
+++ js/common.js
@@ -1,25 +1,23 @@
 //共通パーツ読み込み
 $(function() {
-    window.addEventListener('DOMContentLoaded', function(){
-        window.addEventListener('resize', function(){
-            if (window.innerWidth <= 768　|| navigator.userAgent.match(/iPhone|Android.+Mobile/)) {
-                var link = document.createElement('link');
-                link.href = '/style_m.css';
-                link.rel = 'stylesheet';
-                link.type = 'text/css';
+    window.addEventListener('resize', function(){
+        if (window.innerWidth <= 768　|| navigator.userAgent.match(/iPhone|Android.+Mobile/)) {
+            var link = document.createElement('link');
+            link.href = '/style_m.css';
+            link.rel = 'stylesheet';
+            link.type = 'text/css';
 
-                $("#header").load("/header_m.html");
-                $("#footer").load("/footer_m.html");
-            }
-            else {
-                var link = document.createElement('link');
-                link.href = '/style.css';
-                link.rel = 'stylesheet';
-                link.type = 'text/css';
+            $("#header").load("/header_m.html");
+            $("#footer").load("/footer_m.html");
+        }
+        else {
+            var link = document.createElement('link');
+            link.href = '/style.css';
+            link.rel = 'stylesheet';
+            link.type = 'text/css';
 
-                $("#header").load("/header.html");
-                $("#footer").load("/footer.html");
-            }
-        });
+            $("#header").load("/header.html");
+            $("#footer").load("/footer.html");
+        }
     });
 });
