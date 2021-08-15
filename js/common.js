43d10e72d23927caf43e6191b0300186d08e634b
YotioSoft <ytani0323@gmail.com>
2021-08-15 23:27:30 +0900

    common.jsでstyle.cssを読み込み

--- js/common.js
+++ js/common.js
@@ -1,10 +1,20 @@
 //共通パーツ読み込み
 $(function() {
     if (navigator.userAgent.match(/iPhone|Android.+Mobile/)) {
+		var link = document.createElement('link');
+		link.href = '/style_m.css';
+		link.rel = 'stylesheet';
+		link.type = 'text/css';
+
         $("#header").load("/header_m.html");
         $("#footer").load("/footer_m.html");
     }
     else {
+		var link = document.createElement('link');
+		link.href = '/style.css';
+		link.rel = 'stylesheet';
+		link.type = 'text/css';
+
         $("#header").load("/header.html");
         $("#footer").load("/footer.html");
     }
