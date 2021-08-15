74a44ac43f7ad7797e001d9052640a1865903485
YotioSoft <ytani0323@gmail.com>
2021-08-15 23:53:04 +0900

    モバイル版の適用範囲拡大

--- js/common.js
+++ js/common.js
@@ -1,6 +1,6 @@
 //共通パーツ読み込み
 $(function() {
-    if (window.innerWidth <= 768) {
+    if (window.innerWidth <= 768　|| navigator.userAgent.match(/iPhone|Android.+Mobile/)) {
 		var link = document.createElement('link');
 		link.href = '/style_m.css';
 		link.rel = 'stylesheet';
