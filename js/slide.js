e2bf9d761132b3e2f624d9e363c2c972572e5ba7
YotioSoft <ytani0323@gmail.com>
2021-08-22 16:02:48 +0900

    スライドのテスト

---
+++ js/slide.js
@@ -0,0 +1,33 @@
+$(function() {
+    $('.slide').each(function() {
+      // スライド（画像）の数を取得
+      var $slides = $(this).find('img'),
+          slideNum = $slides.length,
+          currentIdx = 0; // 何番目か
+    
+      // 最初の画像をフェードイン
+      $(".slide p").eq(currentIdx).fadeIn();
+    
+      // 3秒後に次のスライドを表示
+      setTimeout(dispNextSlide, 3000);
+    
+      // 次のスライドを表示するメソッド
+      function dispNextSlide() {
+        var nextIdx = currentIdx + 1;
+    
+        // 最後のスライドの場合ははじめに戻る
+        if (nextIdx > (slideNum - 1)) {
+          nextIdx = 0
+        }
+    
+        // 現在のスライドをフェードアウト
+        $(".slide p").eq(currentIdx).fadeOut();
+    
+        // 次のスライドをフェードイン
+        $(".slide p").eq(nextIdx).fadeIn();
+    
+        // インデックスを更新
+        currentIdx = nextIdx;
+      }
+    });
+});
