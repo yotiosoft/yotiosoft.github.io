274daf2fc5321b1aecc4b199b1862b4009e708c9
YotioSoft <ytani0323@gmail.com>
2021-09-29 16:06:41 +0900

    日付のフォントを調整

--- js/load_article.js
+++ js/load_article.js
@@ -86,7 +86,7 @@ function get_data_for_card(data, arg_blockcard_content, arg_blockcard_tags, arg_
         })
     .then((doc) => {
         arg_blockcard_content.innerHTML = doc.getElementById("article_summary").innerText;
-        arg_blockcard_tags.innerHTML = "<p style=\"width: fit-content; margin-right: 5px;\">" + data.year+"."+formatter.format(data.month)+"."+formatter.format(data.day) +
+        arg_blockcard_tags.innerHTML = "<p style=\"width: fit-content; margin-right: 5px; font-size: 18px;\"><b>" + data.year+"."+formatter.format(data.month)+"."+formatter.format(data.day) + "</b></p>"
             doc.getElementById("article_tags").innerHTML;
         arg_blockcard_title.innerHTML = doc.getElementById("article_title").innerText;
     }
