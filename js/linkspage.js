7118a97fed4ea680d5cc6abd0ef3005ff1331d4a
YotioSoft <ytani0323@gmail.com>
2021-10-18 10:12:41 +0900

    最低でも正方形の高さに変更

--- js/linkspage.js
+++ js/linkspage.js
@@ -8,35 +8,63 @@ while (sb_content_w / sb_row - 10 < sb_card_min_width && sb_row > 1) {
 }
 let sb_card_width = sb_content_w / sb_row - 10;
 document.getElementById('link-sb1').style.width = sb_card_width;
-//document.getElementById('link-sb1').style.height = sb_card_width;
+if (document.getElementById('link-sb1').style.height < sb_card_width) {
+    document.getElementById('link-sb1').style.height = sb_card_width;
+}
 document.getElementById('link-sb2').style.width = sb_card_width;
-//document.getElementById('link-sb2').style.height = sb_card_width;
+if (document.getElementById('link-sb2').style.height < sb_card_width) {
+    document.getElementById('link-sb2').style.height = sb_card_width;
+}
 document.getElementById('link-sb3').style.width = sb_card_width;
-//document.getElementById('link-sb3').style.height = sb_card_width;
+if (document.getElementById('link-sb3').style.height < sb_card_width) {
+    document.getElementById('link-sb3').style.height = sb_card_width;
+}
 document.getElementById('link-sb4').style.width = sb_card_width;
-//document.getElementById('link-sb4').style.height = sb_card_width;
+if (document.getElementById('link-sb4').style.height < sb_card_width) {
+    document.getElementById('link-sb4').style.height = sb_card_width;
+}
 document.getElementById('link-sb5').style.width = sb_card_width;
-//document.getElementById('link-sb5').style.height = sb_card_width;
+if (document.getElementById('link-sb5').style.height < sb_card_width) {
+    document.getElementById('link-sb5').style.height = sb_card_width;
+}
 document.getElementById('link-sb6').style.width = sb_card_width;
-//document.getElementById('link-sb6').style.height = sb_card_width;
+if (document.getElementById('link-sb6').style.height < sb_card_width) {
+    document.getElementById('link-sb6').style.height = sb_card_width;
+}
 document.getElementById('link-sb7').style.width = sb_card_width;
-//document.getElementById('link-sb7').style.height = sb_card_width;
+if (document.getElementById('link-sb7').style.height < sb_card_width) {
+    document.getElementById('link-sb7').style.height = sb_card_width;
+}
 document.getElementById('link-sb8').style.width = sb_card_width;
-//document.getElementById('link-sb8').style.height = sb_card_width;
+if (document.getElementById('link-sb8').style.height < sb_card_width) {
+    document.getElementById('link-sb8').style.height = sb_card_width;
+}
 document.getElementById('link-sb9').style.width = sb_card_width;
-//document.getElementById('link-sb9').style.height = sb_card_width;
+if (document.getElementById('link-sb9').style.height < sb_card_width) {
+    document.getElementById('link-sb9').style.height = sb_card_width;
+}
 
 document.getElementById('link-sbo1').style.width = sb_card_width;
-//document.getElementById('link-sbo1').style.height = sb_card_width;
+if (document.getElementById('link-sbo1').style.height < sb_card_width) {
+    document.getElementById('link-sbo1').style.height = sb_card_width;
+}
 document.getElementById('link-sbo2').style.width = sb_card_width;
-//document.getElementById('link-sbo2').style.height = sb_card_width;
+if (document.getElementById('link-sbo2').style.height < sb_card_width) {
+    document.getElementById('link-sbo2').style.height = sb_card_width;
+}
 
 document.getElementById('link-sbi1').style.width = sb_card_width;
-//document.getElementById('link-sbi1').style.height = sb_card_width;
+if (document.getElementById('link-sbi1').style.height < sb_card_width) {
+    document.getElementById('link-sbi1').style.height = sb_card_width;
+}
 document.getElementById('link-sbi2').style.width = sb_card_width;
-//document.getElementById('link-sbi2').style.height = sb_card_width;
+if (document.getElementById('link-sbi2').style.height < sb_card_width) {
+    document.getElementById('link-sbi2').style.height = sb_card_width;
+}
 document.getElementById('link-sbi3').style.width = sb_card_width;
-//document.getElementById('link-sbi3').style.height = sb_card_width;
+if (document.getElementById('link-sbi3').style.height < sb_card_width) {
+    document.getElementById('link-sbi3').style.height = sb_card_width;
+}
     
 window.addEventListener('resize', function(){
     let sb_default_row = 3;
@@ -49,33 +77,61 @@ window.addEventListener('resize', function(){
     }
     let sb_card_width = sb_content_w / sb_row - 10;
     document.getElementById('link-sb1').style.width = sb_card_width;
-    //document.getElementById('link-sb1').style.height = sb_card_width;
+    if (document.getElementById('link-sb1').style.height < sb_card_width) {
+        document.getElementById('link-sb1').style.height = sb_card_width;
+    }
     document.getElementById('link-sb2').style.width = sb_card_width;
-    //document.getElementById('link-sb2').style.height = sb_card_width;
+    if (document.getElementById('link-sb2').style.height < sb_card_width) {
+        document.getElementById('link-sb2').style.height = sb_card_width;
+    }
     document.getElementById('link-sb3').style.width = sb_card_width;
-    //document.getElementById('link-sb3').style.height = sb_card_width;
+    if (document.getElementById('link-sb3').style.height < sb_card_width) {
+        document.getElementById('link-sb3').style.height = sb_card_width;
+    }
     document.getElementById('link-sb4').style.width = sb_card_width;
-    //document.getElementById('link-sb4').style.height = sb_card_width;
+    if (document.getElementById('link-sb4').style.height < sb_card_width) {
+        document.getElementById('link-sb4').style.height = sb_card_width;
+    }
     document.getElementById('link-sb5').style.width = sb_card_width;
-    //document.getElementById('link-sb5').style.height = sb_card_width;
+    if (document.getElementById('link-sb5').style.height < sb_card_width) {
+        document.getElementById('link-sb5').style.height = sb_card_width;
+    }
     document.getElementById('link-sb6').style.width = sb_card_width;
-    //document.getElementById('link-sb6').style.height = sb_card_width;
+    if (document.getElementById('link-sb6').style.height < sb_card_width) {
+        document.getElementById('link-sb6').style.height = sb_card_width;
+    }
     document.getElementById('link-sb7').style.width = sb_card_width;
-    //document.getElementById('link-sb7').style.height = sb_card_width;
+    if (document.getElementById('link-sb7').style.height < sb_card_width) {
+        document.getElementById('link-sb7').style.height = sb_card_width;
+    }
     document.getElementById('link-sb8').style.width = sb_card_width;
-    //document.getElementById('link-sb8').style.height = sb_card_width;
+    if (document.getElementById('link-sb8').style.height < sb_card_width) {
+        document.getElementById('link-sb8').style.height = sb_card_width;
+    }
     document.getElementById('link-sb9').style.width = sb_card_width;
-    //document.getElementById('link-sb9').style.height = sb_card_width;
+    if (document.getElementById('link-sb9').style.height < sb_card_width) {
+        document.getElementById('link-sb9').style.height = sb_card_width;
+    }
 
     document.getElementById('link-sbo1').style.width = sb_card_width;
-    //document.getElementById('link-sbo1').style.height = sb_card_width;
+    if (document.getElementById('link-sbo1').style.height < sb_card_width) {
+        document.getElementById('link-sbo1').style.height = sb_card_width;
+    }
     document.getElementById('link-sbo2').style.width = sb_card_width;
-    //document.getElementById('link-sbo2').style.height = sb_card_width;
+    if (document.getElementById('link-sbo2').style.height < sb_card_width) {
+        document.getElementById('link-sbo2').style.height = sb_card_width;
+    }
 
     document.getElementById('link-sbi1').style.width = sb_card_width;
-    //document.getElementById('link-sbi1').style.height = sb_card_width;
+    if (document.getElementById('link-sbi1').style.height < sb_card_width) {
+        document.getElementById('link-sbi1').style.height = sb_card_width;
+    }
     document.getElementById('link-sbi2').style.width = sb_card_width;
-    //document.getElementById('link-sbi2').style.height = sb_card_width;
+    if (document.getElementById('link-sbi2').style.height < sb_card_width) {
+        document.getElementById('link-sbi2').style.height = sb_card_width;
+    }
     document.getElementById('link-sbi3').style.width = sb_card_width;
-    //document.getElementById('link-sbi3').style.height = sb_card_width;
+    if (document.getElementById('link-sbi3').style.height < sb_card_width) {
+        document.getElementById('link-sbi3').style.height = sb_card_width;
+    }
 });
