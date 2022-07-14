0057be485aa07dbf26fbc225f71fe633ff1b04aa
YotioSoft <ytani0323@gmail.com>
2022-07-14 22:57:45 +0900

    scripts for each page in common.js

--- js/common.js
+++ js/common.js
@@ -1,3 +1,14 @@
+// gtag
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+gtag('config', 'UA-174934210-1');
+
+// for mobile devices
+if ((navigator.userAgent.indexOf('iPhone') > 0) || navigator.userAgent.indexOf('iPod') > 0 || navigator.userAgent.indexOf('Android') > 0) {
+    document.write('<meta name="viewport" content="width=device-width, initial-scale=1.0">');
+}
+
 //共通パーツ読み込み
 $(function() {
     if (window.innerWidth <= 767　|| navigator.userAgent.match(/iPhone|Android.+Mobile/)) {
