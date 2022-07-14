d4a5a9b520d4672b9ebb1e29dab7b6a2738e703c
YotioSoft <ytani0323@gmail.com>
2022-07-15 00:01:06 +0900

    rm console log

--- js/side.js
+++ js/side.js
@@ -11,7 +11,9 @@ $(function($){
     var sticked_height = sticked.height();
 
     $(window).on('resize', function(){          // リサイズ時
+        sidebar_top = sidebar.offset().top;
         sticked_original_top = sticked.offset().top;
+        sticked_height = sticked.height();
     });
 
     $(window).on('scroll resize', function(){ // スクロールかリサイズ時
@@ -29,7 +31,6 @@ $(function($){
         var content_bottom = content.offset().top + content.height();
 
         if ((scrollTop >= sticked_original_top) && (scrollTop < content_bottom - sticked_height)){
-            console.log(`scrollTop: ${scrollTop}, sticked_original_top: ${sticked_original_top}, content_bottom: ${content_bottom}, sticked_height: ${sticked_height}`);
             // 現在位置が、初期位置より下で、メインコンテンツ最後尾より上なら、画面上部にサイドバーを固定
             sticked.css({
                 'position': 'fixed',
