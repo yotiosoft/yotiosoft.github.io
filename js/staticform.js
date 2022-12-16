9e330fa0a55cd6b888dcbb30a0bcbf8295658647
YotioSoft <ytani0323@gmail.com>
2022-12-16 23:34:45 +0900

    replace getting api key

--- js/staticform.js
+++ js/staticform.js
@@ -1,9 +1,12 @@
 function put_input_obj() {
+  /*
   fetch('https://evening-savannah-53967.herokuapp.com/static-forms-apikey')
   .then(response => response.text())
   .then(function(data) {
     document.getElementById("form_input").value = data;
   });
+  */
+  document.getElementById("form_input").value = "86128fb5-a309-431d-a5ab-6d3152b0a036";
 }
 
 // find elements
